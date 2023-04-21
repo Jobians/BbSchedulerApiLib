d45117f797975412921efbd988de56fd0c58d688
Jobians Techie <88005779+Jobians@users.noreply.github.com>
2023-04-21 09:58:41 +0100

    Update SchedulerApi.js

--- libs/SchedulerApi.js
+++ libs/SchedulerApi.js
@@ -7,6 +7,19 @@
 let libPrefix = "SchedulerApi";
 let API_URL = "https://api.jobians.top/runafter.php";
 
+function sendRequest(data) {
+    var bot_token = bot.token
+    var headers = {
+        "Content-type": "application/json"
+    }
+    HTTP.post({
+        url: API_URL,
+        success: libPrefix + 'ongetResult',
+        body: data,
+        headers: headers
+    })
+}
+
 function create(options) {
     if (!Libs.Webhooks) {
         throw "Please install Webhook Lib from the Store. It is required by SchedulerApi Lib"
@@ -20,29 +33,20 @@ function create(options) {
     if (!options.command) {
         throw "SchedulerApi Lib: need options.command"
     }
-    var bot_token = bot.token
-    var headers = {
-        "Content-type": "application/json"
-    }
     var webhook = Libs.Webhooks.getUrlFor({
         command: options.command,
         user_id: user.id
     })
     var data = {
-        bot_token: bot_token,
+        bot_token: bot.token,
         webhook: webhook,
         minutes: options.minutes,
         parameters: options.parameters,
         label: options.label
     }
-    HTTP.post({
-        url: API_URL,
-        success: libPrefix + 'ongetResult',
-        body: data,
-        headers: headers
-    })
+    sendRequest(data);
 }
-/*
+
 function cancel(options) {
     if (!options) {
         throw "SchedulerApi Lib: need options"
@@ -50,23 +54,14 @@ function cancel(options) {
     if (!options.id && !options.label) {
         throw "SchedulerApi Lib: need options.id or options.label"
     }
-    var bot_token = bot.token
-    var headers = {
-        "Content-type": "application/json"
-    }
     var data = {
-        bot_token: bot_token,
+        bot_token: bot.token,
         cancel: true,
         id: options.id,
         label: options.label
     }
-    HTTP.post({
-        url: API_URL,
-        success: libPrefix + 'ongetResult',
-        body: data,
-        headers: headers
-    })
-}*/
+    sendRequest(data);
+}
 
 function ongetResult(content) {
     let info = JSON.parse(content);
@@ -74,11 +69,11 @@ function ongetResult(content) {
     if (status == "error") {
         throw "SchedulerApi Lib: " + info.message;
     }
-    return info;
+    return status;
 }
 
 on(libPrefix + 'ongetResult', ongetResult);
 publish({
     create: create,
-    //cancel: cancel
+    cancel: cancel
 })
